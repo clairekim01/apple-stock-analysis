cd5b1361a5a2032d54dbca4e72b00343c5abf624
Yara Younis <98911660+yarayounis@users.noreply.github.com>
2022-12-07 20:35:03 -0500

    Update index.js

--- stockSite/src/pages/index.js
+++ stockSite/src/pages/index.js
@@ -10,7 +10,15 @@ return (
 		height: '100vh'
 	}}
 	>
-	<h4>Visualizations</h4>
+	<body>
+	<header>
+		<h4>Visualizations</h4>
+	</header>
+	<p>Open stock price: first price a stock trades at when the market opens</p>
+	<p>High stock price: more people want to buy a stock than sell it, supply is lower than demand</p>
+	<p>Low stock price: more people want to sell a stock than buy it, supply is greater than demand</p>
+	<p>Close stock price: last price a stock trades at when the market closes</p>
+	</body>
 	</div>
 );
 };
