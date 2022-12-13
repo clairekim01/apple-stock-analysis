e3d6e385889aba462c98dfc88a0754bb1726bedc
Claire Kim <clairekim247@gmail.com>
2022-12-13 00:03:18 -0500

    added all visualizations to home

--- stockSite/src/pages/index.js
+++ stockSite/src/pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Helmet } from "react-helmet";
-import MyImage1 from '../../images/vis1';
-import MyImage2 from '../../images/vis2';
-import MyImage3 from '../../images/vis3';
-import MyImage4 from '../../images/vis4';
-import MyImage5 from '../../images/vis5';
-import MyImage6 from '../../images/vis6';
-import MyImage7 from '../../images/vis7';
-import MyImage8 from '../../images/vis8';
+import MyImage1 from '../images/vis1.png';
+import MyImage2 from '../images/vis2.png';
+import MyImage3 from '../images/vis3.png';
+import MyImage4 from '../images/vis4.png';
+import MyImage5 from '../images/vis5.png';
+import MyImage6 from '../images/vis6.png';
+import MyImage7 from '../images/vis7.png';
+import MyImage8 from '../images/vis8.png';
 
 const Home = () => {
 return (
@@ -45,14 +45,14 @@ return (
 				<script src="https://public.flourish.studio/resources/embed.js"></script>
 			</Helmet>
 		</div>
-		<img src={MyImage1} alt="vis1" width="700" height="600" />
-		<img src={MyImage2} alt="vis2" width="700" height="600"/>
-		<img src={MyImage3} alt="vis3" width="700" height="600"/>
-		<img src={MyImage4} alt="vis4" width="700" height="600" />
-		<img src={MyImage5} alt="vis5" width="700" height="600" />
-		<img src={MyImage6} alt="vis6" width="700" height="600" />
-		<img src={MyImage7} alt="vis7" width="700" height="600" />
-		<img src={MyImage8} alt="vis8" width="700" height="600"/>
+		<img src={MyImage1} alt="vis1" width="580px" height="400px"/>
+		<img src={MyImage2} alt="vis2" width="580px" height="400px"/>
+		<img src={MyImage3} alt="vis3" width="580px" height="400px"/>
+		<img src={MyImage4} alt="vis4" width="580px" height="400px" />
+		<img src={MyImage5} alt="vis5" width="580px" height="400px" />
+		<img src={MyImage6} alt="vis6" width="580px" height="400px" />
+		<img src={MyImage7} alt="vis7" width="580px" height="400px" />
+		<img src={MyImage8} alt="vis8" width="580px" height="400px"/>
 	</div>
 
 	</body>
